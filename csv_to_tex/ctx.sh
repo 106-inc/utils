1a6d250c6076b57bee4d4f5927a4d1f98aaea981
derzhavin3016 <derzhavin.andrej@yandex.ru>
2020-10-11 01:07:34 +0300

    Bash script deleted & executable made

--- csv_to_tex/ctx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-py_filename="csv_to_tex.py"
-if [ -e $py_filename ] # Check if the python source file exists
-then python3 $py_filename $* 
-else
-echo "ctx: error: cannot found file '$py_filename'"
-fi
-
