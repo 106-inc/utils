5d6434820cc6327dd35b010167a53d7c5ebd4015
Tako-San <dxe13@mail.ru>
2020-10-19 23:34:35 +0300

    my

--- tmp_dir_for_huawei/main.cpp
+++ tmp_dir_for_huawei/main.cpp
@@ -84,99 +84,110 @@ public:
         Word funct3 = GetBits<14,12>(wrd);
 
 
-        if (GetBits<6, 0>(wrd) == 0x33)
+        switch (GetBits<6, 0>(wrd))
         {
-            Word funct7 = GetBits<31, 25>(wrd);
+            case 0x33:
+                Word funct7 = GetBits<31, 25>(wrd);
 
-            if (funct3 == 0 && funct7 == 0)
-                m_insn = ADD;
-            else if (funct3 == 0 && funct7 == 0x20)
-                m_insn = SUB;
-            else if (funct3 == 0x3 && funct7 == 0)
-                m_insn = XOR;
-            else if (funct3 == 0x5 && funct7 == 0)
-                m_insn = OR;
+                if (funct3 == 0 && funct7 == 0)
+                    m_insn = ADD;
+                else if (funct3 == 0 && funct7 == 0x20)
+                    m_insn = SUB;
+                else if (funct3 == 0x3 && funct7 == 0)
+                    m_insn = XOR;
+                else if (funct3 == 0x5 && funct7 == 0)
+                    m_insn = OR;
+
+                break;
+
+            case 0x03:
+                m_imm = GetBits<31, 20>(wrd);
+
+                switch (funct3)
+                {
+                    case 0x00:
+                        m_insn = LB;
+                        break;
+                    case 0x01:
+                        m_insn = LH;
+                        break;
+                    case 0x02:
+                        m_insn = LW;
+                        break;
+                    case 0x04:
+                        m_insn = LBU;
+                        break;
+                    case 0x05:
+                        m_insn = LHU;
+                        break;
+                    default:
+                        std::cout << "Unknown command\n";
+                }
+
+                break;
+
+            case 0x63:
+
+                m_imm = (GetBits<31, 20>(wrd) << 5) | GetBits<11,7>(wrd);
+
+                switch (funct3)
+                {
+                    case 0x00:
+                        m_insn = BEQ;
+                        break;
+                    case 0x01:
+                        m_insn = BNE;
+                        break;
+                    case 0x04:
+                        m_insn = BNE;
+                        break;
+                    case 0x05:
+                        m_insn = BGE;
+                        break;
+                    case 0x06:
+                        m_insn = BLTU;
+                        break;
+                    default:
+                        std::cout << "Unknown command\n";
+                }
+
+                break;
+
+            case 0x13:
+
+                m_imm = GetBits<31, 20>(wrd);
+
+                switch (funct3)
+                {
+                    case 0x00:
+                        m_insn = ADDI;
+                        break;
+                    case 0x01:
+                        m_insn = SLTI;
+                        break;
+                    case 0x03:
+                        m_insn = SLTIU;
+                        break;
+                    case 0x04:
+                        m_insn = XORI;
+                        break;
+                    case 0x05:
+                        m_insn = ORI;
+                        break;
+                    case 0x06:
+                        m_insn = ANDI;
+                    default:
+                        std::cout << "Unknown command\n";
+                }
+
+                break;
+
+            default:
+
+                std::cout << "Unknown command\n";
         }
 
-        if (GetBits<6, 0>(wrd) == 0x03)
-        {
-            m_imm = GetBits<31, 20>(wrd);
 
-            switch (funct3)
-            {
-                case 0x00:
-                    m_insn = LB;
-                    break;
-                case 0x01:
-                    m_insn = LH;
-                    break;
-                case 0x02:
-                    m_insn = LW;
-                    break;
-                case 0x04:
-                    m_insn = LBU;
-                    break;
-                case 0x05:
-                    m_insn = LHU;
-                    break;
-                default:
-                    std::cout << "Unknown command\n";
-            }
-        }
-
-        if (GetBits<6, 0>(wrd) == 0x63)
-        {
-            m_imm = (GetBits<31, 20>(wrd) << 5) | GetBits<11,7>(wrd);
-
-            switch (funct3)
-            {
-                case 0x00:
-                    m_insn = BEQ;
-                    break;
-                case 0x01:
-                    m_insn = BNE;
-                    break;
-                case 0x04:
-                    m_insn = BNE;
-                    break;
-                case 0x05:
-                    m_insn = BGE;
-                    break;
-                case 0x06:
-                    m_insn = BLTU;
-                    break;
-                default:
-                    std::cout << "Unknown command\n";
-            }
-        }
-
-        if (GetBits<6, 0>(wrd) == 0x13)
-        {
-            m_imm = GetBits<31, 20>(wrd);
-
-            switch (funct3)
-            {
-                case 0x00:
-                    m_insn = ADDI;
-                    break;
-                case 0x01:
-                    m_insn = SLTI;
-                    break;
-                case 0x03:
-                    m_insn = SLTIU;
-                    break;
-                case 0x04:
-                    m_insn = XORI;
-                    break;
-                case 0x05:
-                    m_insn = ORI;
-                    break;
-                case 0x06:
-                    m_insn = ANDI;
-                default:
-                    std::cout << "Unknown command\n";
-            }
-        }
     }
 
 
