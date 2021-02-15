6b594efa4fb2f71e018b8674aaee9b21b2d0bb25
andrey <derzhavin.andrej@yandex.ru>
2021-02-15 19:53:17 +0300

    new functions added

--- angles/angles.py
+++ angles/angles.py
@@ -1,22 +1,69 @@
 #!/usr/bin/python3
 
+import math
+
 class Angle:
 
-    def __init__(self, deg=0, mins=0, secs=0):
-        self.deg_ = deg
-        self.min_ = mins
-        self.sec_ = secs
+    def __init__(self, degs=0, mins=0, secs=0):
+        sec_sum = secs
+        min_sum = mins + sec_sum // 60
+        deg_sum = degs + min_sum // 60
 
-    def __add__(self, other):
+        self.sec_ = sec_sum % 60
+        self.min_ = min_sum % 60
+        self.deg_ = deg_sum
+
+    def __iadd__(self, other):
         sec_sum = self.sec_ + other.sec_
         min_sum = self.min_ + other.min_ + (sec_sum // 60)
         deg_sum = self.deg_ + other.deg_ + (min_sum // 60)
 
-        return Angle(deg_sum, min_sum % 60, sec_sum % 60)
+        self.sec_ = sec_sum % 60
+        self.min_ = min_sum % 60
+        self.deg_ = deg_sum
+
+        return self
+
+    def __add__(self, other):
+        temp = self
+        temp += other
+
+        return temp
+
+    def __isub__(self, other):
+        """
+        TODO: write
+        """
+        return self
+
+    def __sub__(self, other):
+        """
+        TODO: write
+        """
+        return self
+
+    def __neg__(self):
+        """
+        TODO: write
+        """
+        return self
 
     def __str__(self):
         return f"{self.deg_}°{self.min_}'{self.sec_}''"
 
+    def __float__(self):
+        return self.degrees()
 
-a = Angle(10, 34, 3)
-print(a)
+    def degrees(self):
+        return self.deg_ + self.min_ / 60 + self.sec_ / 3600
+
+    def radians(self):
+        return self.degrees() / 180 * math.pi
+
+
+a = Angle(30)
+b = a + a
+
+print('a = ', a)
+print(b.degrees())
+print(b.radians())
